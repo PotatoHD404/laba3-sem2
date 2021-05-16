932726e2df2b655e79f69a9cca421674d249b13f
PotatoHD404 <31506993+PotatoHD404@users.noreply.github.com>
2021-05-16 23:41:27 +0300

    Added some pwa things, wont use them for some reasons

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-const withWorkers = require('@zeit/next-workers')
 module.exports = {
     webpack(config, {isServer}) {
         config.output.webassemblyModuleFilename = 'static/build/[modulehash].build'
@@ -23,3 +22,31 @@ module.exports = {
         return config
     },
 }
+// const withPWA = require('next-pwa')
+// module.exports = withPWA({
+//     webpack(config, {isServer}) {
+//         config.output.webassemblyModuleFilename = 'static/build/[modulehash].build'
+//         config.module.rules.push({
+//                 test: /\.(wasm)$/,
+//                 loader: 'file-loader',
+//                 type: 'javascript/auto',
+//                 options: {
+//                     outputPath: 'static',
+//                 },
+//             }
+//
+//             // Overcome Webpack referencing `window` in chunks
+//
+//         )
+//         config.output.globalObject = `(typeof self !== 'undefined' ? self : this)`
+//         if (!isServer) {
+//             config.node = {
+//                 fs: 'empty'
+//             }
+//         }
+//         return config
+//     },
+//     pwa: {
+//         dest: "public"
+//     }
+// })
